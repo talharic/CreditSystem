c25fb5d881210df0cafdf50906b4b48ad80fdf1a
Talha Arıç <talharic@gmail.com>
2022-02-21 21:48:05 +0300

    Show Credit Application

--- credit-system-frontend/src/api/credit/CreditApplicationService.js
+++ credit-system-frontend/src/api/credit/CreditApplicationService.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-class CreditApplicationService{
+class CreditApplicationService {
+  saveCreditApplication(newCreditApplication) {
+    var url = "api/v1/credit-applications";
 
-    saveCreditApplication(newCreditApplication){
-        var url = "api/v1/credit-applications";
+    return axios.post(url, newCreditApplication);
+  }
 
-        return axios.post(url, newCreditApplication);
-    }
+  showCreditApplication(nationalIdNumber) {
+    var url = "api/v1/credit-applications/";
 
+    return axios.get(url, {
+      params: {
+        id: nationalIdNumber
+      },
+    });
+  }
 }
 
 export default new CreditApplicationService();
--- credit-system-frontend/src/components/credit/ShowCreditApplicationPage.js
+++ credit-system-frontend/src/components/credit/ShowCreditApplicationPage.js
@@ -1,17 +1,92 @@
 import React from "react";
 import PageTitle from "../pageTitle/PageTitle";
+import serialize from "form-serialize";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import CreditApplicationService from "../../api/credit/CreditApplicationService";
+import Credit from "./Credit";
 
 class ShowCreditApplicationPage extends React.Component {
+  state = {
+    creditApplicationResponse: [],
+    errorMessage: "",
+    showAlert : false,
+    showForm: true,
+    showResult: false
+  };
 
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const data = serialize(e.target, { hash: true })
 
-    render() {
+    const nationalIdNumber = data.nationalIdNumber;
+    //console.log(nationalIdNumber)
+    this.showApplication(nationalIdNumber)
+  };
 
-        return (
-            <>
+  showApplication(nationalIdNumber) {
+    CreditApplicationService.showCreditApplication(nationalIdNumber)
+      .then((response) => this.handleResponse(response))
+      .catch((error) => this.handleError(error));
+  }
+
+  handleResponse(response) {
+      console.log(response)
+    this.setState({ creditApplicationResponse: response.data, showAlert: false, showForm:false, showResult: true });
+  }
+
+  handleError(error) {
+    if (error === undefined) return;
+    this.setState({ errorMessage: error.response.data.errors.join(" ") , showAlert: true,showForm:true , showResult: false });
+    console.log(error.response.data);
+  }
+
+  clearForm() {
+    document.getElementById("apply-credit-form").reset();
+  }
+
+  render() {
+    return (
+      <>
                 <PageTitle title="Show Credit Application"></PageTitle>
-            </>
-        )
-    }
+        <div className="container col-md-6 offset-md-3">
+        { this.state.showForm ?  
+          <Form
+            id="apply-credit-form"
+            className="mt-5"
+            onSubmit={this.handleFormSubmit}
+          >
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="nationalIdNumber">
+                National Id Number
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="nationalIdNumber"
+                maxLength={11}
+                minLength={11}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Apply
+            </Button>
+          </Form>
+          : null }
+          {this.state.errorMessage && (
+            <Alert variant="danger" className="mb-3 mt-3" show={this.state.showAlert}>
+              <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+              <p>{this.state.errorMessage}</p>
+            </Alert>
+          )}
+          {this.state.showResult ? (
+            <Credit items={this.state.creditApplicationResponse} />
+          ) : null}
+        </div>
+      </>
+    );
+  }
 }
 
 export default ShowCreditApplicationPage;
